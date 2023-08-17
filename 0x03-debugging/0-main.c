aec2fa6c99a2b0cc726f058760481c1d03f6e1e4
Mohamed Ahmed Negm <83051202+negmeme2001@users.noreply.github.com>
2023-08-17 19:55:11 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
         int i;
@@ -13,3 +13,4 @@ int main(void)
 
         return (0);
 }
+
