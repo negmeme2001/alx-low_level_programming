7c6313d7ea1167da6c5d2ab9ccd16e678682595a
Mohamed Ahmed Negm <83051202+negmeme2001@users.noreply.github.com>
2023-08-17 19:56:02 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
-/**
- * positive _or_negative : print if the number is positive or negative
- * 
- * 
- */
+
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
