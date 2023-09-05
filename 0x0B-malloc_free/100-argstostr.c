8102e12792ae8d8f1ac951490f3937beff218478
Mohamed Ahmed Negm <83051202+negmeme2001@users.noreply.github.com>
2023-09-05 18:36:22 +0300

    fixes

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,44 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
+#include "_strlen.c"
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
- * argstostr - gives array of args
- * @ac: argument count
- * @av: array of arguments
- * Return: char value
+ * argstostr - concatenate all args
+ * @ac: argc
+ * @av: arguments
+ *
+ * Return: array
  */
+
 char *argstostr(int ac, char **av)
 {
-	int size;
 	char *s;
-	int i;
-	int j;
-	int k;
+	int len = 0, i, j, k = 0;
 
 	if (ac == 0 || av == NULL)
+	{
 		return (NULL);
-
+	}
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			size++;
+		len += _strlen(av[i]);
 	}
-
-	size += (ac + 1);
-	s = malloc(sizeof(char) * size);
+	len += (ac + 1);
+	s = malloc(len * sizeof(char));
 	if (s == NULL)
-		return (NULL);
-	k = 0;
-	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			s[k] = av[i][j];
-			k++;
-		}
-		s[k] = '\n';
-		k++;
+		free(s);
+		return (NULL);
 	}
 
-	s[k] = '\0';
-
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			s[k++] = av[i][j];
+		}
+		s[k++] = '\n';
+	}
 	return (s);
 }
